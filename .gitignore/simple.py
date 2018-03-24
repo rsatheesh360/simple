b0da4f966722936f9e1521fea9a4ad850db29300
R.Satheesh <36471895+rsatheesh360@users.noreply.github.com>
2018-03-24 14:28:16 +0530

    total amount

---
+++ .gitignore/simple.py
@@ -0,0 +1,6 @@
+amt=int(input("Enter Amount:"))
+inter=int(input("Enter interest:"))
+period=int(input("Enter Time(Month):"))
+totamt=amt*(inter/100)*period
+print("Total amount is:",round(totamt))
+print("total amount")
